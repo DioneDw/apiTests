c0e5ceec0fdc1678f563735b35b32fffdc6fd2b7
DioneDw <dionewilly@hotmail.com>
2025-03-05 22:20:43 -0300

    User class

--- apiTests/src/main/java/com/dwprojects/apiTests/ApiTestsApplication.java
+++ apiTests/src/main/java/com/dwprojects/apiTests/ApiTestsApplication.java
@@ -1,5 +1,6 @@
 package com.dwprojects.apiTests;
 
+import com.dwprojects.apiTests.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class ApiTestsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestsApplication.class, args);
+
+		User user = new User(1,"Teste","teste@teste.com","12345");
 	}
 
 }
