a4e4b3bd5fd26025d7c1a1d15e39cb8ac549da1f
DioneDw <dionewilly@hotmail.com>
2025-03-10 18:52:14 -0300

    Test for class user service method findById

---
+++ apiTests/src/test/java/com/dwprojects/apiTests/services/impl/UserServiceImplTest.java
@@ -0,0 +1,81 @@
+package com.dwprojects.apiTests.services.impl;
+
+import com.dwprojects.apiTests.domain.User;
+import com.dwprojects.apiTests.domain.dto.UserDTO;
+import com.dwprojects.apiTests.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final String NAME = "DioneTest";
+    public static final String EMAIL = "dioneteste@teste.com";
+    public static final String PASSWORD = "1234";
+    public static final int ID = 1;
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(User.class, response.getClass());
+        Assertions.assertEquals(ID, response.getId());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
