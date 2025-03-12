82c4f4706500529872c93715799764d289c8eac3
DioneDw <dionewilly@hotmail.com>
2025-03-11 21:39:14 -0300

    Test for class user service method create with success

--- apiTests/src/test/java/com/dwprojects/apiTests/services/impl/UserServiceImplTest.java
+++ apiTests/src/test/java/com/dwprojects/apiTests/services/impl/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -90,8 +91,22 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess(){
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
+
+
     }
+
 
     @Test
     void update() {
